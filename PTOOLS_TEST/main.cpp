a98501c4eabe4af594722a73143e1d649154d4c5
putao520 <yuyao1022@hotmail.com>
2021-07-28 13:22:48 +0800

    更新DLL匹配新版本，完善例子说明

--- PTOOLS_TEST/main.cpp
+++ PTOOLS_TEST/main.cpp
@@ -11,41 +11,48 @@ typedef void (WINAPI* def_change_step)(ULONG step);
 void recv_status(ULONG step) {
 	printf("状态:%d\n", step);
 	if (step == 6) {// 成功
+		// 打开进程获得进程句柄
 		void* handle = LoadProcess(::GetCurrentProcessId());
 		if( !handle )
 			return;
 		Sleep(500);
 		cout << "Create Handle:" << handle << endl;
 		_PTR moduleBase = 0;
+		// 获得进程内模块基址
 		GetModuleBase(handle, "ntdll.dll", &moduleBase);
 		printf("Ntdll.dll:%#llx\n", moduleBase);
 		_PTR buffer = 0;
+		// 申请进程内存
 		AllocProcessMemory(handle, 64, true, &buffer);
 		printf("Buffer:%#llx\n", buffer);
 		char test[] = "Test!";
+		// 写入进程内存
 		PutProcessMemory(handle, (void*)buffer, test, 6);
 		printf("Write from:%s to:%#llx\n", test, buffer);
 		char recv[64] = { 0 };
+		// 读取内存
 		GetProcessMemory(handle, (void*)buffer, recv, 6);
 		printf("Read  from:%s to:%s\n", (char *)buffer, recv);
+		// 释放内存
 		FreeProcessMemory(handle, (void*)buffer);
 		cout << "Free buffer:" << buffer << endl;
-
-		// 创建物理内存读写句柄
+		// 创建物理内存专用读写句柄
 		HANDLE local_handle = CreateLocalHandle(handle);
 		cout << "Create Local Handle:" << local_handle << endl;
 		// 新申请一个内存用来测试
 		buffer = 0;
 		AllocProcessMemory(handle, 64, true, &buffer);
-		
+		// 关闭基础句柄
 		UnloadProcess(handle);
 		cout << "Close Handle:" << handle << endl;
-		
+		// 使用专用读写句柄写内存
 		PutProcessMemory(local_handle, (void*)buffer, test, 6);
 		printf("[py]Write from:%s to:%#llx\n", test, buffer);
 		memset(recv, 0, 64);
+		// 使用专用读写句柄读内存
 		GetProcessMemory(local_handle, (void*)buffer, recv, 6);
 		printf("[py]Read  from:%s to:%s\n", (char*)buffer, recv);
+		// 关闭专用读写句柄
 		CloseLocalHandle(local_handle);
 		cout << "Close Local Handle:" << local_handle << endl;
 	}
